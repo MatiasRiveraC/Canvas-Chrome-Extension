528b8d865406bdedad3f5ae64e6c19c2708a5100
root <root@DESKTOP-8BUGFQ5.localdomain>
2021-08-28 16:25:47 -0400

    currently working

--- content.js
+++ content.js
@@ -1,16 +1,28 @@
 
-document.addEventListener("DOMContentLoaded", setDarmode());
+document.addEventListener("DOMContentLoaded", setDarkmode());
 
-function setDarmode(){
+function setDarkmode(){
     
 chrome.runtime.sendMessage({msg: "darkMode"}, function(response){
     let darkMode = response.msg;
     console.log(darkMode);
   
     if(darkMode){
+      
+
+      let extras = document.querySelectorAll(".ic-DashboardCard__header_image, .ic-DashboardCard__header_hero, .avatar");
+      extras.forEach((item) => {
+        item.style.filter = "invert(1) hue-rotate(180deg)"; 
+      });
+
+      let header = document.getElementById("header");
+      header.style.filter = "invert(1) hue-rotate(180deg)"; 
+
       document.querySelector("body").style.filter = "invert(1) hue-rotate(180deg)";
       document.body.style.backgroundColor = "#000000";
+      
     }
 
   });
 }
+
--- popup.js
+++ popup.js
@@ -1,30 +1,61 @@
 // Initialize button with user's preferred color
 let darkMode = document.getElementById("darkMode");
-
+let profile = document.getElementById("profile");
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 darkMode.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: setDarkMode,
     });
 });
 
+profile.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: redirect_profile,
+  });
+});
+
+
+function redirect_profile(){
+  window.location.href ="https://uandes.instructure.com/profile";
+  console.log("REDIRECTING");
+}
+
 
 function setDarkMode(){
   chrome.runtime.sendMessage({msg: "darkMode"}, function(response){
     let darkMode = response.msg;
+    let extras = document.querySelectorAll(".ic-DashboardCard__header_image, .ic-DashboardCard__header_hero, .avatar ");
+    let header = document.getElementById("header");
+      
   
     if(!darkMode){
       document.querySelector("body").style.filter = "invert(1) hue-rotate(180deg)";
       document.body.style.backgroundColor = "#000000";
+      
+      
+      extras.forEach((item) => {
+        item.style.filter = "invert(1) hue-rotate(180deg)"; 
+      });
+      
+      header.style.filter = "invert(1) hue-rotate(180deg)"; 
+
       chrome.runtime.sendMessage({msg: "turnOn"}, function(response){});
     }
     else{
       document.querySelector("body").style.filter = "invert(0)";
       document.body.style.backgroundColor = "#ffffff";
+
+      extras.forEach((item) => {
+        item.style.filter = "invert(0) hue-rotate(0deg)"; 
+      });
+
+      header.style.filter = "invert(0) hue-rotate(0deg)"; 
+
       chrome.runtime.sendMessage({msg: "turnOff"}, function(response){});
     }
 
